63ec2b1bc2335e5bfc154e965b1ead248f117134
ShenSouza <ds052798ts@gmail.com>
2021-02-23 18:35:36 -0300

    Subindo projeto

---
+++ farmasaude/src/main/java/com/generation/farmasaude/FarmasaudeApplication.java
@@ -0,0 +1,13 @@
+package com.generation.farmasaude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmasaudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmasaudeApplication.class, args);
+	}
+
+}
